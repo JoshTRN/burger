dabad4c0d447c777c9cedf1d34aca7bce16fd79b
Joshua Wood <joshua@Joshuas-iMac.local>
2018-04-27 20:41:03 -0400

    making initial commits

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var connection = require('./connection.js');
+
+function SQL() {
+    this.selectAll = function() {
+
+    };
+    this.insertOne = function() {
+
+    };
+    this.updateOne = function () {
+
+    };
+}
+console.log('required');
+module.exports = SQL;
---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var sql = require('./config/orm.js');
+
+console.log(sql);
